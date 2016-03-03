b2fbcc9aeea2d4478c75d9e8192f149b235bbeff
Zakaria <zakaria.ra@gmail.com>
2016-03-03 10:21:52 +0100

    Page scores

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,4 +1,12 @@
 angular.module('app', [])
-    .controller('mastermindController', function($scope){
+    .controller('mastermindController', function($scope, $http, $filter){
        $scope.title = "Hello me"; 
+       $scope.scores = [];
+              
+        setInterval(function() {
+            $http.get('http://uc399925:5555/api/mastermind/Scors/')
+            .then(function(result) {
+                $scope.scores = result.data; 
+            });
+        }, 5000);
     });
